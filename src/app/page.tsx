4c2d989ec1c31a0e0aee1de7a5e0a9d042257b6c
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-11-10 15:50:46 +0900

    style: 랭킹 페이지 퍼블리싱

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
+'use client';
+
 import React from 'react'
 import FluentEmojiTrophy from "@/icons/FluentEmojiTrophy";
+import {useRouter} from "next/navigation";
 
 export default function Home() {
-  return (
-      <main className={'flex flex-col justify-between bg-[#141414] min-h-screen px-5'}>
+  const router = useRouter();
+    return (
+      <main className={'dark flex flex-col justify-between bg-background min-h-screen px-5'}>
           <div className={'mt-5 flex justify-between rounded-full bg-gray-800 opacity-90 px-4 py-3 items-center'}>
               <div className={'flex items-center gap-x-1 text-gray-300'}><FluentEmojiTrophy />친구들 중 1등이에요</div>
-              <button className={'text-white text-[14px] px-3 py-1 rounded-[16px] bg-gray-600'}>랭킹 확인하기</button>
+              <button
+                  onClick={() => {
+                      router.push('/rank');
+                  }}
+                  className={'text-white text-[14px] px-3 py-1 rounded-[16px] bg-gray-600'}>랭킹 확인하기</button>
           </div>
           <div className={'flex flex-col gap-y-8 justify-center items-center'}>
               <div className={'text-white text-[18px]'}>내 최고 기록 <span className={'text-[12px]'}>| </span> 55점</div>

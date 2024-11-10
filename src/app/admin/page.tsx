f645078122ccd5a6689671a796ad57236e13c928
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-11-10 16:07:33 +0900

    style: 관리자 페이지 퍼블리싱

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,132 @@
+import React from "react";
+
+const AdminPage = () => {
+    return (
+        <div className={'dark bg-background min-h-screen'}>
+            <div className={'mx-5'}>
+                <div className={'pt-5 text-white text-[24px]'}>사용자 정보</div>
+                <div className={'border-b border-gray-600 px-3 py-4'}>
+                    <div className={'flex justify-between items-center'}>
+                        <div className={'flex gap-x-5'}>
+                            <div className={'text-[18px] text-red-400'}>
+                                1
+                            </div>
+                            <div>
+                                <div className={'text-white text-[14px]'}>황유림 / 2021070015</div>
+                                <div className={'text-gray-400 text-[14px]'}>55점</div>
+                            </div>
+                        </div>
+                        <button className={'text-white text-[14px] px-3 py-1 rounded-[16px] bg-gray-600'}>추가하기
+                        </button>
+                    </div>
+                </div>
+                <div className={'border-b border-gray-600 px-3 py-4'}>
+                    <div className={'flex justify-between items-center'}>
+                        <div className={'flex gap-x-5'}>
+                            <div className={'text-[18px] text-red-400'}>
+                                2
+                            </div>
+                            <div>
+                                <div className={'text-white text-[14px]'}>황유림 / 2021070015</div>
+                                <div className={'text-gray-400 text-[14px]'}>55점</div>
+                            </div>
+                        </div>
+                        <button className={'text-white text-[14px] px-3 py-1 rounded-[16px] bg-gray-600'}>추가하기
+                        </button>
+                    </div>
+                </div>
+                <div className={'border-b border-gray-600 px-3 py-4'}>
+                    <div className={'flex justify-between items-center'}>
+                        <div className={'flex gap-x-5'}>
+                            <div className={'text-[18px] text-red-400'}>
+                                3
+                            </div>
+                            <div>
+                                <div className={'text-white text-[14px]'}>황유림 / 2021070015</div>
+                                <div className={'text-gray-400 text-[14px]'}>55점</div>
+                            </div>
+                        </div>
+                        <button className={'text-white text-[14px] px-3 py-1 rounded-[16px] bg-gray-600'}>추가하기
+                        </button>
+                    </div>
+                </div>
+                <div className={'border-b border-gray-600 px-3 py-4'}>
+                    <div className={'flex justify-between items-center'}>
+                        <div className={'flex gap-x-5'}>
+                            <div className={'text-[18px] text-red-400'}>
+                                4
+                            </div>
+                            <div>
+                                <div className={'text-white text-[14px]'}>황유림 / 2021070015</div>
+                                <div className={'text-gray-400 text-[14px]'}>55점</div>
+                            </div>
+                        </div>
+                        <button className={'text-white text-[14px] px-3 py-1 rounded-[16px] bg-gray-600'}>추가하기
+                        </button>
+                    </div>
+                </div>
+                <div className={'border-b border-gray-600 px-3 py-4'}>
+                    <div className={'flex justify-between items-center'}>
+                        <div className={'flex gap-x-5'}>
+                            <div className={'text-[18px] text-red-400'}>
+                                5
+                            </div>
+                            <div>
+                                <div className={'text-white text-[14px]'}>황유림 / 2021070015</div>
+                                <div className={'text-gray-400 text-[14px]'}>55점</div>
+                            </div>
+                        </div>
+                        <button className={'text-white text-[14px] px-3 py-1 rounded-[16px] bg-gray-600'}>추가하기
+                        </button>
+                    </div>
+                </div>
+                <div className={'border-b border-gray-600 px-3 py-4'}>
+                    <div className={'flex justify-between items-center'}>
+                        <div className={'flex gap-x-5'}>
+                            <div className={'text-[18px] text-red-400'}>
+                                6
+                            </div>
+                            <div>
+                                <div className={'text-white text-[14px]'}>황유림 / 2021070015</div>
+                                <div className={'text-gray-400 text-[14px]'}>55점</div>
+                            </div>
+                        </div>
+                        <button className={'text-white text-[14px] px-3 py-1 rounded-[16px] bg-gray-600'}>추가하기
+                        </button>
+                    </div>
+                </div>
+                <div className={'border-b border-gray-600 px-3 py-4'}>
+                    <div className={'flex justify-between items-center'}>
+                        <div className={'flex gap-x-5'}>
+                            <div className={'text-[18px] text-red-400'}>
+                                7
+                            </div>
+                            <div>
+                                <div className={'text-white text-[14px]'}>황유림 / 2021070015</div>
+                                <div className={'text-gray-400 text-[14px]'}>55점</div>
+                            </div>
+                        </div>
+                        <button className={'text-white text-[14px] px-3 py-1 rounded-[16px] bg-gray-600'}>추가하기
+                        </button>
+                    </div>
+                </div>
+                <div className={'border-b border-gray-600 px-3 py-4'}>
+                    <div className={'flex justify-between items-center'}>
+                        <div className={'flex gap-x-5'}>
+                            <div className={'text-[18px] text-red-400'}>
+                                8
+                            </div>
+                            <div>
+                                <div className={'text-white text-[14px]'}>황유림 / 2021070015</div>
+                                <div className={'text-gray-400 text-[14px]'}>55점</div>
+                            </div>
+                        </div>
+                        <button className={'text-white text-[14px] px-3 py-1 rounded-[16px] bg-gray-600'}>추가하기
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default AdminPage;

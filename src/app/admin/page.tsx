60c42b1a358d4dcb688ef968d737b860c577832a
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-11-13 09:57:54 +0900

    feat: admin 페이지 횟수 증가 버튼 반응형 추가
    *feat: button스타일링 변경, 버튼 클릭시 데이터 다시 불러오기

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { getBonusChance } from '@/lib/api/rank';
+import {getBonusChance} from '@/lib/api/rank';
 import { getUserList } from '@/lib/api/admin';
 import Cookies from 'js-cookie';
 
@@ -10,11 +10,20 @@ const AdminPage = () => {
 
   useEffect(() => {
     getUserList().then((r) => {
-      // 데이터 덮어쓰기 (prevState 사용 X)
       setUserList(r.data.data);
-      console.log('r', r.data.data);
     });
   }, []); // 빈 의존성 배열로 컴포넌트 마운트 시 한 번만 실행
+
+    const handleBonusChance = (ssn: number) => {
+        const adminSsn = JSON.parse(Cookies.get('user') || '{}').ssn;
+        getBonusChance(ssn, adminSsn).then(() => {
+            // getRankList로 사용자 정보 가져오기
+            getUserList().then((r) => {
+                console.log('업데이트된 사용자 정보', r);
+                setUserList(r.data.data); // 상태 업데이트
+            });
+        });
+    };
 
     return (
         <div className={'dark bg-background min-h-screen'}>
@@ -30,15 +39,14 @@ const AdminPage = () => {
                                     </div>
                                     <div>
                                         <div className={'text-white text-[14px]'}>{user.name} / {user.ssn} / {user.phoneNumber}</div>
-                                        <div className={'text-gray-400 text-[14px]'}>{user.bestScore}점</div>
+                                        <div className={'text-gray-400 text-[14px]'}>{user.tryCount}회 / {user.bestScore}점</div>
                                     </div>
                                 </div>
                                 <button
                                     onClick={() => {
-                                        //TODO: 학번 번호 바꾸기
-                                        getBonusChance(user.ssn, JSON.parse(Cookies.get('user') || '{}').ssn)
+                                        handleBonusChance(user.ssn)
                                     }}
-                                    className={'text-white text-[14px] px-3 py-1 rounded-[16px] bg-gray-600'}>추가하기
+                                    className={'text-white text-[14px] px-3 py-1 rounded-[16px] bg-gray-600 active:bg-gray-700 hover:scale-110 transition'}>추가하기
                                 </button>
                             </div>
                         </section>
--- src/lib/api/rank.ts
+++ src/lib/api/rank.ts
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
 export const getBonusChance = (ssn: number, admin: number) => {
-    axios
-        .get(`https://draw-o.kro.kr/api/v1/member/bonus?ssn=${ssn}&admin=${admin}`)
-        .then((res) => {
-            console.log('보너스 기회 제공 get', res);
-        });
+    return axios.get(`https://draw-o.kro.kr/api/v1/member/bonus?ssn=${ssn}&admin=${admin}`)
+
 };
 export const getRankList = (ssn: number) => {
     return axios.get(`https://draw-o.kro.kr/api/v1/rank/show/${ssn}`)
